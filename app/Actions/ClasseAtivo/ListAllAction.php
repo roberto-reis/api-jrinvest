51cfc49e859d36bb3ab71d1d0fff973a1d5b619e
José Roberto <tekinforroberto@gmail.com>
2023-04-09 11:20:53 -0300

    feat: Criando estrutura basica para classe ativo

---
+++ app/Actions/ClasseAtivo/ListAllAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Actions\ClasseAtivo;
+
+class ListAllAction
+{
+    public function __invoke(array $filters): array
+    {
+
+        return [];
+    }
+}
---
+++ app/Repositories/ClasseAtivoRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories;
+
+class ClasseAtivoRepository
+{
+    public function __construct()
+    {
+
+    }
+}
