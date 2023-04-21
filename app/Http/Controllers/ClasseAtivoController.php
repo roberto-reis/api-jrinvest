a0f7c454c010d87492999d5e97819df8b53b0ec3
José Roberto <tekinforroberto@gmail.com>
2023-04-21 20:00:31 -0300

    feat: Implementando o método store no controller de classes ativo

--- app/Http/Controllers/ClasseAtivoController.php
+++ app/Http/Controllers/ClasseAtivoController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\JsonResponse;
+use App\Actions\ClasseAtivo\StoreAction;
 use App\Actions\ClasseAtivo\ListAllAction;
 use App\Http\Requests\ClasseAtivo\ListClasseAtivoRequest;
+use App\Http\Requests\ClasseAtivo\StoreClasseAtivoRequest;
 
 class ClasseAtivoController extends Controller
 {
-    public function listAll(ListClasseAtivoRequest $request, ListAllAction $listAll)
+    public function listAll(ListClasseAtivoRequest $request, ListAllAction $listAll): JsonResponse
     {
         try {
             $classesAtivos = $listAll->execute($request->validated());
@@ -22,6 +24,25 @@ class ClasseAtivoController extends Controller
             send_log('Erro ao listar as classes de ativos', [], 'error', $e);
             return response()->json([
                 'menssage' => 'Erro ao listar as classes de ativos',
+                'data' => []
+            ], 500);
+        }
+    }
+
+    public function store(StoreClasseAtivoRequest $request, StoreAction $storeAction): JsonResponse
+    {
+        try {
+            $classesAtivos = $storeAction->execute($request->validated());
+
+            return response()->json([
+                'menssage' => 'Dados cadastrados com sucesso',
+                'data' => $classesAtivos
+            ], 201);
+
+        } catch (\Exception $e) {
+            send_log('Erro ao cadastrar a classe de ativo', [], 'error', $e);
+            return response()->json([
+                'menssage' => 'Erro ao cadastrar a classe de ativo',
                 'data' => []
             ], 500);
         }
