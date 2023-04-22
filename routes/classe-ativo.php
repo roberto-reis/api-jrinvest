38d08bcc20941258643a7d6df83e2b51c3892785
José Roberto <tekinforroberto@gmail.com>
2023-04-22 15:20:59 -0300

    feat: Criando rota de update classe de ativo

--- routes/classe-ativo.php
+++ routes/classe-ativo.php
@@ -7,5 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::controller(ClasseAtivoController::class)->prefix('classe-ativo')->name('classe-ativo')->group(function() {
     Route::get('list-all', 'listAll')->name('.listAll');
     Route::post('store', 'store')->name('.store');
+    Route::put('update/{uid}', 'update')->name('.update');
 });
 
