51a05864ca2df14649b82041f29b4f494537777a
José Roberto <tekinforroberto@gmail.com>
2023-04-22 15:22:13 -0300

    feat: Implementando no controller metodo de atualizar classe de ativo

--- app/Http/Controllers/ClasseAtivoController.php
+++ app/Http/Controllers/ClasseAtivoController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use App\Actions\ClasseAtivo\StoreAction;
+use App\Actions\ClasseAtivo\UpdateAction;
 use App\Actions\ClasseAtivo\ListAllAction;
+use App\Exceptions\ClasseAtivoNaoEncontradoException;
 use App\Http\Requests\ClasseAtivo\ListClasseAtivoRequest;
 use App\Http\Requests\ClasseAtivo\StoreClasseAtivoRequest;
+use App\Http\Requests\ClasseAtivo\UpdateClasseAtivoRequest;
 
 class ClasseAtivoController extends Controller
 {
@@ -25,7 +28,7 @@ class ClasseAtivoController extends Controller
             return response()->json([
                 'menssage' => 'Erro ao listar as classes de ativos',
                 'data' => []
-            ], 500);
+            ], $e->getCode() == 0 ? 500 : $e->getCode());
         }
     }
 
@@ -44,7 +47,31 @@ class ClasseAtivoController extends Controller
             return response()->json([
                 'menssage' => 'Erro ao cadastrar a classe de ativo',
                 'data' => []
-            ], 500);
+            ], $e->getCode() == 0 ? 500 : $e->getCode());
+        }
+    }
+
+    public function update(UpdateClasseAtivoRequest $request, UpdateAction $updateAction, $uid): JsonResponse
+    {
+        try {
+            $classesAtivos = $updateAction->execute($uid, $request->validated());
+
+            return response()->json([
+                'menssage' => 'Dados Atualizados com sucesso',
+                'data' => $classesAtivos
+            ], 200);
+
+        } catch (ClasseAtivoNaoEncontradoException $e) {
+            return response()->json([
+                'menssage' => $e->getMessage(),
+                'data' => []
+            ], $e->getCode());
+        } catch (\Exception $e) {
+            send_log('Erro ao atualizar a classe de ativo', [], 'error', $e);
+            return response()->json([
+                'menssage' => 'Erro ao atualizar a classe de ativo',
+                'data' => []
+            ], $e->getCode() == 0 ? 500 : $e->getCode());
         }
     }
 }
