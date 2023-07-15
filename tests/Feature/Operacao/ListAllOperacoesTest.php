beab09f5af1dcfd8d0f4eabc5431548ec72a440e
José Roberto <tekinforroberto@gmail.com>
2023-07-15 19:32:04 -0300

    test: Criando teste automatizado para feature listar operações de um usuário

--- tests/Feature/Operacao/ListAllOperacoesTest.php
+++ tests/Feature/Operacao/ListAllOperacoesTest.php
@@ -25,34 +25,32 @@ class ListAllOperacoesTest extends TestCase
         $response->assertStatus(200);
     }
 
-    // public function test_deve_listar_proventos_com_filtro_paginate(): void
-    // {
-    //     $this->withMiddleware();
-    //     $user = User::factory()->create();
-    //     Sanctum::actingAs($user, ['*']);
-    //     Provento::factory(5)->create(['user_uid' => $user->uid]);
-    //     $perPage = 2;
+    public function test_deve_listar_operacoes_com_filtro_paginate(): void
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user, ['*']);
+        Operacao::factory(5)->create(['user_uid' => $user->uid]);
+        $perPage = 2;
 
-    //     $response = $this->get(route('proventos.listAll', [
-    //         'perPage' => $perPage
-    //     ]));
+        $response = $this->get(route('operacoes.listAll', [
+            'perPage' => $perPage
+        ]));
 
-    //     $response->assertStatus(200);
-    //     assertEquals($response->json()['data']['per_page'], $perPage);
-    // }
+        $response->assertStatus(200);
+        assertEquals($response->json()['data']['per_page'], $perPage);
+    }
 
-    // public function test_deve_esta_autenticado_para_listar_todos_proventos_de_um_usuario(): void
-    // {
-    //     $this->withMiddleware();
-    //     $uidQualquer = '32c0e209-cff9-4cc3-af17-71cb6a48d01a';
+    public function test_deve_esta_autenticado_para_listar_todos_operacoes_de_um_usuario(): void
+    {
+        $uidQualquer = '32c0e209-cff9-4cc3-af17-71cb6a48d01a';
 
-    //     $response = $this->get(route('proventos.listAll', $uidQualquer), [
-    //         'Accept' => 'application/json'
-    //     ]);
+        $response = $this->get(route('operacoes.listAll', $uidQualquer), [
+            'Accept' => 'application/json'
+        ]);
 
-    //     $response->assertStatus(401)
-    //              ->assertJson([
-    //                 'message' => 'Unauthenticated.'
-    //              ]);
-    // }
+        $response->assertStatus(401)
+                 ->assertJson([
+                    'message' => 'Unauthenticated.'
+                 ]);
+    }
 }
