f588e972b2707b449c4298bc14919abe87d35dae
José Roberto <tekinforroberto@gmail.com>
2023-11-05 12:12:37 -0300

    feat: Criado action de fogot para user

--- app/Exceptions/AuthException.php
+++ app/Exceptions/AuthException.php
@@ -4,6 +4,6 @@ namespace App\Exceptions;
 
 use Exception;
 
-class UserException extends Exception
+class AuthException extends Exception
 {
 }
--- app/Interfaces/Repositories/IAuthRepository.php
+++ app/Interfaces/Repositories/IAuthRepository.php
@@ -9,4 +9,6 @@ interface IAuthRepository
     public function store(UserDto $dto): array;
     public function exists(string $value, string $field = 'uid'): bool;
     public function update(string $uid, UserDto $dto): array;
+    public function storePasswordResetToken(array $dados): array;
+    public function deleteToken(string $email): bool;
 }
