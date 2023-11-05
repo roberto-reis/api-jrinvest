62bef372e5bbdfe6d7184bf33e09f2cb6a9ad7d0
José Roberto <tekinforroberto@gmail.com>
2023-11-05 12:10:11 -0300

    feat: Implementado os metodos storePasswordResetToken e deleteToken no repository

---
+++ app/Models/PasswordResetToken.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UuidTrait;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PasswordResetToken extends Model
+{
+    use UuidTrait;
+    use HasFactory;
+
+    protected $table = 'password_reset_tokens';
+    protected $primaryKey = 'email';
+    protected $keyType = 'string';
+    protected $icrementing = false;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'email',
+        'token',
+        'created_at'
+    ];
+}
--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -4,7 +4,8 @@ namespace App\Repositories;
 
 use App\Models\User;
 use App\DTOs\Auth\UserDto;
-use App\Exceptions\UserException;
+use App\Exceptions\AuthException;
+use App\Models\PasswordResetToken;
 use App\Interfaces\Repositories\IAuthRepository;
 
 class AuthRepository implements IAuthRepository
@@ -33,11 +34,23 @@ class AuthRepository implements IAuthRepository
         $user = $this->model::find($uid);
 
         if (!$user) {
-            throw new UserException('User não encontrado', 404);
+            throw new AuthException('User não encontrado', 404);
         }
 
         $user->update($dto->toArray());
 
         return $user->toArray();
+    }
+
+    public function storePasswordResetToken(array $dados): array
+    {
+        $passwordResetToken = PasswordResetToken::create($dados);
+
+        return $passwordResetToken->toArray();
+    }
+
+    public function deleteToken(string $email): bool
+    {
+        return PasswordResetToken::where('email', $email)->delete();
     }
 }
